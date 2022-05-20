030d59a3b36db1708b56748aea0e5726d89fa7b8
Edward Zarecor <ed@tcril.org>
2022-05-20 11:40:10 -0400

    fix: support GH signin

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -34,8 +34,28 @@ import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { PermissionedRoute } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
 
+// Start Changes for GitHub Auth
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
+import { SignInProviderConfig, SignInPage } from '@backstage/core-components';
+
+const githubProvider: SignInProviderConfig = {
+  id: 'github-auth-provider',
+  title: 'GitHub',
+  message: 'Sign in using GitHub',
+  apiRef: githubAuthApiRef,
+};
+// End changes for GitHub Auth
 const app = createApp({
   apis,
+  components: {
+  SignInPage: props => (
+    <SignInPage
+      {...props}
+      auto
+      provider={githubProvider}
+    />
+    ),
+  },  
   bindRoutes({ bind }) {
     bind(catalogPlugin.externalRoutes, {
       createComponent: scaffolderPlugin.routes.root,
