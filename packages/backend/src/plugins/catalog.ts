f0e7bf097ab2df6fa07e98b735f6d7b1e965478a
Edward Zarecor <ed@tcril.org>
2022-06-06 11:18:54 -0400

    fix: adding support for GitHub catalog discovery

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,7 +2,20 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
-import { GitHubOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
+import { GitHubOrgEntityProvider,
+         GithubDiscoveryProcessor,
+         GithubOrgReaderProcessor,
+       } from '@backstage/plugin-catalog-backend-module-github';
+
+// GitHub Discovery: https://backstage.io/docs/integrations/github/discovery
+// import {
+//   GithubDiscoveryProcessor,
+//   GithubOrgReaderProcessor,
+// } from '@backstage/plugin-catalog-backend-module-github';
+import {
+  ScmIntegrations,
+  DefaultGithubCredentialsProvider
+} from '@backstage/integration';
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -24,6 +37,21 @@ export default async function createPlugin(
   );
 
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+
+  const integrations = ScmIntegrations.fromConfig(env.config);
+  const githubCredentialsProvider =
+    DefaultGithubCredentialsProvider.fromIntegrations(integrations);
+  builder.addProcessor(
+    GithubDiscoveryProcessor.fromConfig(env.config, {
+      logger: env.logger,
+      githubCredentialsProvider,
+    }),
+    GithubOrgReaderProcessor.fromConfig(env.config, {
+      logger: env.logger,
+      githubCredentialsProvider,
+    }),
+  );
+
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
