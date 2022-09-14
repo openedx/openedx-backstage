3f34869b2df2b45bd9ee427f4a650f9f8beaac4e
Feanil Patel <feanil@tcril.org>
2022-09-14 09:01:27 -0400

    feat: Update how we pull entities from github.
    Instead of using the processors(GithubDiscoveryProcessor and
    GithOrgReaderProcessor) we use providers.
    
    The reasoning can be found here: https://backstage.io/docs/integrations/github/org#using-a-processor-instead-of-a-provider
    
    > An alternative to using the Provider for ingesting organizational
    > entities is to use a Processor. This is the old way that's based on
    > registering locations with the proper type and target, triggering the
    > processor to run.
    >
    > The drawback of this method is that it will leave orphaned Group/User
    > entities whenever they are deleted on your GitHub server, and you cannot
    > control the frequency with which they are refreshed, separately from
    > other processors.
    
    So since processors are the old way and can leave orphans, we move to
    the new way which is via providers.
    
    This also has the added benefit that we can refresh the users and groups
    in the org less often since they don't change as quickly which should
    also help with some of our rate limiting issues.

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,17 +2,14 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
-import {
-  GitHubOrgEntityProvider,
-  GithubDiscoveryProcessor,
-  GithubOrgReaderProcessor,
-} from '@backstage/plugin-catalog-backend-module-github';
 
-// GitHub Discovery: https://backstage.io/docs/integrations/github/discovery
-// import {
-//   GithubDiscoveryProcessor,
-//   GithubOrgReaderProcessor,
-// } from '@backstage/plugin-catalog-backend-module-github';
+/* ADDED BASED ON Multiple Pages */
+import {
+  GitHubEntityProvider, // ADDED BASED ON https://backstage.io/docs/integrations/github/discovery
+  GitHubOrgEntityProvider, // ADDED BASED ON https://backstage.io/docs/integrations/github/org
+} from '@backstage/plugin-catalog-backend-module-github';
+/* END ADDITION */
+
 import {
   ScmIntegrations,
   DefaultGithubCredentialsProvider,
@@ -23,6 +20,28 @@ export default async function createPlugin(
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
 
+  /* ADDED BASED ON https://backstage.io/docs/integrations/github/discovery
+   *
+   * This is for pulling in repos from any org that is defined in the
+   * `catalog/providers/github` list that have a catalog-info.yaml.
+   */
+  builder.addEntityProvider(
+    GitHubEntityProvider.fromConfig(env.config, {
+      logger: env.logger,
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { minutes: 60 },
+        timeout: { minutes: 15 },
+      }),
+    }),
+  );
+  /* END ADDITION */
+
+  /* ADDED BASED ON https://backstage.io/docs/integrations/github/org
+   *
+   * This will map backstage User and Group entities to match the users and
+   * groups in our github org.
+   */
+
   // The org URL below needs to match a configured integrations.github entry
   // specified in your app-config.
   builder.addEntityProvider(
@@ -31,28 +50,15 @@ export default async function createPlugin(
       orgUrl: 'https://github.com/openedx',
       logger: env.logger,
       schedule: env.scheduler.createScheduledTaskRunner({
-        frequency: { minutes: 60 },
+        frequency: { minutes: 120 },
         timeout: { minutes: 15 },
       }),
     }),
   );
+  /* END ADDITION */
+
 
   builder.addProcessor(new ScaffolderEntitiesProcessor());
-
-  const integrations = ScmIntegrations.fromConfig(env.config);
-  const githubCredentialsProvider =
-    DefaultGithubCredentialsProvider.fromIntegrations(integrations);
-  builder.addProcessor(
-    GithubDiscoveryProcessor.fromConfig(env.config, {
-      logger: env.logger,
-      githubCredentialsProvider,
-    }),
-    GithubOrgReaderProcessor.fromConfig(env.config, {
-      logger: env.logger,
-      githubCredentialsProvider,
-    }),
-  );
-
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
