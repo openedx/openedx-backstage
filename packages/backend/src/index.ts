37afb86511b27a6817a65e1a9457c9133dfb68aa
Feanil Patel <feanil@axim.org>
2024-08-16 13:18:55 -0400

    feat: Add packages to be able to pull users from the org.
    In order for user entities to get populated automatically from the
    github org we need a User entity catalog provide.  This one pulls user
    data from github orgs it has access to and creates user entities for
    them in the backstage database.
    
    We add the new plugin and install it in the backend system and add
    config for it into the app-config.yaml file.

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -20,16 +20,15 @@ backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
 backend.add(import('@backstage/plugin-auth-backend'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 // See https://backstage.io/docs/auth/guest/provider
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend/alpha'));
-backend.add(
-  import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
-);
-backend.add(import('@backstage/plugin-catalog-backend/alpha'));
+backend.add(import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'));
 backend.add(import('@backstage/plugin-catalog-backend-module-github/alpha'));
 backend.add(eventsModuleGithubEventRouter());
+backend.add(import('@backstage/plugin-catalog-backend-module-github-org'));
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
